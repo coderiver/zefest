7e17276eca2c9fe80c7fb9609b1f034d4625253a
Yevgen <godun.yevgen@gmail.com>
2013-01-23 16:58:40 +0200

    index.html (part1)

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+    $(".js-enter").click(function(){
+        $(".js-popup-enter").fadeIn();
+    });
+    $(".js-close-popup").click(function(){
+        $(".js-popup-enter").fadeOut();
+    });
 });
