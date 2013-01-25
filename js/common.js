1a12183e9e59f4652a6a90d54a6aeb283bfa6c3a
Yevgen <godun.yevgen@gmail.com>
2013-01-25 10:45:14 +0200

    index.html(part3)

--- js/common.js
+++ js/common.js
@@ -13,6 +13,10 @@ $(document).ready(function() {
       next:'.js-next-partn',
       prev:'.js-prev-partn'
     });
+    $(".js-scroll-cons").scrollable({
+      next:'.js-next-cons',
+      prev:'.js-prev-cons'
+    });
     if ($(".js-scroll-partn").length>0) {
      // Get the Scrollable control
      var scrollable_list_1 = $(".js-scroll-partn").data("scrollable");
@@ -22,7 +26,31 @@ $(document).ready(function() {
      scrollable_list_1.onSeek(function(event, index) {
        // Check to see if we're at the end
        if (this.getIndex() >= this.getSize() - number_list) {      // Disable the Next link
-         $(".icon_next").addClass("disabled");
+         $(".js-next-partn").addClass("disabled");
+       }
+     });
+     // Handle the Scrollable control's onBeforeSeek event
+     scrollable_list_1.onBeforeSeek(function(event, index) {
+       // Check to see if we're at the end
+       if (this.getIndex() >= this.getSize() - number_list) { 
+         // Check to see if we're trying to move forward
+         if (index > this.getIndex()) {
+           // Cancel navigation
+           return false;
+         }
+       }
+     });
+   }
+   if ($(".js-scroll-cons").length>0) {
+     // Get the Scrollable control
+     var scrollable_list_1 = $(".js-scroll-cons").data("scrollable");
+     // Set to the number of visible items
+     var number_list = 4;
+     // Handle the Scrollable control's onSeek event
+     scrollable_list_1.onSeek(function(event, index) {
+       // Check to see if we're at the end
+       if (this.getIndex() >= this.getSize() - number_list) {      // Disable the Next link
+         $(".js-next-cons").addClass("disabled");
        }
      });
      // Handle the Scrollable control's onBeforeSeek event
